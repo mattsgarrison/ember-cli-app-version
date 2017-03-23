b31bb75610970c9c5bc8bbd985c01e435ff1cc29
Matt Garrison <mgarrison@educationalmeasures.com>
2017-03-23 08:30:30 -0600

    Init repo

---
+++ README.md
@@ -0,0 +1,7 @@
+## ember-cli-app-version
+
+This is a tool for generating a file into your Ember build artifacts which
+
+## Thanks
+
+This was heavily cribbed from [broccoli-manifest](https://github.com/racido/broccoli-manifest).
---
+++ index.js
@@ -0,0 +1,46 @@
+var path = require('path');
+var fs   = require('fs');
+var mergeTrees = require('broccoli-merge-trees');
+var funnel = require('broccoli-funnel');
+
+var appVersion = require('./lib/app-version');
+
+module.exports = {
+  name: 'ember-cli-app-version',
+
+  isDevelopingAddon: function() {
+    return true;
+  },
+
+  config: function (env, baseConfig) {
+    var options = baseConfig.appVersion || {};
+
+    var defaultOptions = {
+      enabled: true,
+      version: '0.0.0',
+      showCreateDate: true,
+      outputFile: '/current_version.txt'
+    }
+
+    for (var option in defaultOptions) {
+      if (!options.hasOwnProperty(option)) {
+        options[option] = defaultOptions[option];
+      }
+    }
+
+    this.appVersionOptions = options;
+  },
+
+  postprocessTree: function (type, tree) {
+    var options = this.appVersionOptions;
+
+    if (options.enabled) {
+      appVersionTree = funnel(tree, {});
+      return mergeTrees([tree, appVersion(appVersionTree, options)], {overwrite: true});
+    }
+
+    return tree;
+  },
+
+  treeFor: function() {}
+}
